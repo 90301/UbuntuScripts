afbacd355f6c59a181a46cb66c8dfe056d5854b9
Joshua Benton <josh@styleconnect.net>
2018-05-31 23:11:36 -0400

    First Comitt

---
+++ basicSetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# your code goes here
+
+sudo add-apt-repository ppa:webupd8team/java
+
+sudo apt-get update
+
+sudo apt-get install oracle-java8-installer -y
+
+sudo apt-get install mysql-server -y
+
+wget http://www.trieuvan.com/apache/tomcat/tomcat-9/v9.0.8/bin/apache-tomcat-9.0.8.tar.gz
+
+tar -xzvf apache-tomcat-9.0.8.tar.gz
+mv apache-tomcat-9.0.8/ tomcat/
