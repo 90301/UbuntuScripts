1e2d465278f9ab80d9fc4b774b2c691517bc6fc4
Joshua Benton <josh@styleconnect.net>
2018-06-03 15:52:33 -0400

    Update basicSetup.sh
    Added Tomcat init.d support

--- basicSetup.sh
+++ basicSetup.sh
@@ -12,7 +12,12 @@ sudo apt-get install mysql-server -y
 wget http://www.trieuvan.com/apache/tomcat/tomcat-9/v9.0.8/bin/apache-tomcat-9.0.8.tar.gz
 
 tar -xzvf apache-tomcat-9.0.8.tar.gz
-mv apache-tomcat-9.0.8/ tomcat/
+mv apache-tomcat-9.0.8/ ~/tomcat/
 rm ./apache-tomcat-9.0.8.tar.gz
+
+sudo cp ./tomcat9 /etc/init.d/tomcat9
+
+chmod 755 /etc/init.d/tomcat9
+update-rc.d tomcat9 defaults
 
 sudo apt-get install ssh -y
