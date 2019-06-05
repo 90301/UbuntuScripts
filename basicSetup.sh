dc7c243695f0a4e791c5872da9176a2ffbd52fd8
Benton <josh.benton@duckcreek.com>
2019-06-05 10:40:20 -0400

    more Jenkins Tweaking

--- basicSetup.sh
+++ basicSetup.sh
@@ -23,6 +23,6 @@ sudo apt-get install ssh -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt install jenkins -y
+sudo apt install jenkins -y  --allow-unauthenticated
 #sed -i 's/original/new/g' file.txt
 sed -i 's/8080/8081/g' /etc/default/jenkins
