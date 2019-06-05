60b53bb8b9b26ef9ca5ca4a8afaad0e363bb9272
Benton <josh.benton@duckcreek.com>
2019-06-05 11:40:04 -0400

    Cert Script Fixed for 18.04

--- installCert.sh
+++ installCert.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 #setupCert
-BASE_DIR="/usr/local/share/ca-certificates/"
+BASE_DIR="/usr/share/ca-certificates/"
+CERT_NAME=
+# Update /etc/ca-certificates.conf
+echo 'c1.cer' >> '/etc/ca-certificates.conf'
 
 sudo cp ./c1.cer $BASE_DIR/c1.cer
-sudo chmod 644 $BASE_DIR/c1.cer
+sudo chmod +rwx $BASE_DIR/c1.cer
 sudo update-ca-certificates
