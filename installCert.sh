9bd55a14a0e8bf7ea6e5572a834b96f9cbe7fe42
Benton <josh.benton@duckcreek.com>
2019-06-05 11:09:50 -0400

    cert update

--- installCert.sh
+++ installCert.sh
@@ -2,8 +2,7 @@
 
 #setupCert
 BASE_DIR="/usr/local/share/ca-certificates/"
-sudo mkdir $BASE_DIR/dc
-sudo chmod 755 $BASE_DIR/dc
-cp ./c1.cer $BASE_DIR/dc/c1.cer
-sudo chmod 644 $BASE_DIR/dc/c1.cer
+
+sudo cp ./c1.cer $BASE_DIR/c1.cer
+sudo chmod 644 $BASE_DIR/c1.cer
 sudo update-ca-certificates
