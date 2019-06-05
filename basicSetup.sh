71af07b1b7e2d768bf8ed0705cdc93ac756c9e32
Benton <josh.benton@duckcreek.com>
2019-06-05 10:27:05 -0400

    updated basic setup to newest tomcat version 9.0.20

--- basicSetup.sh
+++ basicSetup.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 # your code goes here
+
+TOMCAT_VERSION="9.0.20"
 
 sudo add-apt-repository ppa:webupd8team/java
 
@@ -9,11 +11,11 @@ sudo apt-get install oracle-java8-installer -y
 
 sudo apt-get install mysql-server -y
 
-wget http://www.trieuvan.com/apache/tomcat/tomcat-9/v9.0.8/bin/apache-tomcat-9.0.8.tar.gz
+wget http://www.trieuvan.com/apache/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz
 
-tar -xzvf apache-tomcat-9.0.8.tar.gz
-mv apache-tomcat-9.0.8/ ~/tomcat/
-rm ./apache-tomcat-9.0.8.tar.gz
+tar -xzvf apache-tomcat-$TOMCAT_VERSION.tar.gz
+mv apache-tomcat-$TOMCAT_VERSION/ ~/tomcat/
+rm ./apache-tomcat-$TOMCAT_VERSION.tar.gz
 
 sudo cp ./tomcat9 /etc/init.d/tomcat9
 
@@ -21,3 +23,10 @@ chmod 755 /etc/init.d/tomcat9
 update-rc.d tomcat9 defaults
 
 sudo apt-get install ssh -y
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
+#sed -i 's/original/new/g' file.txt
+sed -i 's/8080/8081/g' /etc/default/jenkins
