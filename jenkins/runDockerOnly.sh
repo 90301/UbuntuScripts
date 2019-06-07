c06bee9729e061f76ec83e1e3813afc07467dbad
Josh Benton <tbsw94@gmail.com>
2019-06-07 11:05:42 -0400

    attempt to add jars mountpoint

--- jenkins/runDockerOnly.sh
+++ jenkins/runDockerOnly.sh
@@ -1,5 +1,5 @@
 cd ./jenkins
 docker build . -t styleconnect-jenkins -f ./Dockerfile
-docker run  -d -p 8081:8080 -v jenkins_home:/jenkins_home --name styleconnect-jenkins styleconnect-jenkins
+docker run  -d -p 8081:8080 -v jenkins_home:/jenkins_home -v jars:/jars --name styleconnect-jenkins styleconnect-jenkins
 cd ../
 # -P [HOST]:[CONTAINER]
