c0aa06ffd10a96f6a8b9864322ea24342912a850
Josh Benton <tbsw94@gmail.com>
2019-06-07 10:19:37 -0400

    jenkins more attempts

--- jenkins/runDockerOnly.sh
+++ jenkins/runDockerOnly.sh
@@ -1,3 +1,4 @@
-docker run -p 8081:8080 -v jenkins_home:/jenkins_home --name styleconnect-jenkins
-
+cd ./jenkins
+docker run  -d -p 8081:8080 -v jenkins_home:/jenkins_home --name styleconnect-jenkins ./DockerFile
+cd ../
 # -P [HOST]:[CONTAINER]
