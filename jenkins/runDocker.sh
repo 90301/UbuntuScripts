26ca012c00ad3fe83228e279d5042544ec98af96
Josh Benton <tbsw94@gmail.com>
2019-06-09 07:46:03 -0400

    changed to mount syntax

--- jenkins/runDocker.sh
+++ jenkins/runDocker.sh
@@ -1,3 +1,6 @@
 mkdir /jenkins_home/
 mkdir /jars/
-docker run  -d -p 8081:8080 -v /jenkins_home:/var/jenkins_home -v /jars:/jars --name styleconnect-jenkins styleconnect-jenkins
+docker run  -d -p 8081:8080 \
+    --mount source=/jenkins_home,target=/var/jenkins_home \
+    --mount source=/jars,target=/jars \
+    --name styleconnect-jenkins styleconnect-jenkins
