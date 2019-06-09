bf802d1033490d7a236799b35f5ca8018ba0731e
Josh Benton <tbsw94@gmail.com>
2019-06-09 07:26:31 -0400

    changed folder mount syntax

--- jenkins/runDockerOnly.sh
+++ jenkins/runDockerOnly.sh
@@ -1,5 +1,7 @@
 cd ./jenkins
+mkdir /jenkins_home/
+mkdir /jars/
 docker build . -t styleconnect-jenkins -f ./Dockerfile
-docker run  -d -p 8081:8080 -v jenkins_home:/jenkins_home -v jars:/jars --name styleconnect-jenkins styleconnect-jenkins
+docker run  -d -p 8081:8080 -v /jenkins_home:/var/jenkins_home -v /jars:/jars --name styleconnect-jenkins styleconnect-jenkins
 cd ../
 # -P [HOST]:[CONTAINER]
