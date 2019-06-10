42f6c8565dc8f182672bb5e7c54bce8aeda52b44
Josh Benton <tbsw94@gmail.com>
2019-06-09 22:13:02 -0400

    Convert bind mounts to volumes

--- jenkins/runDocker.sh
+++ jenkins/runDocker.sh
@@ -1,6 +1,6 @@
 mkdir /jenkins_home/
 mkdir /jars/
 docker run  -d -p 8081:8080 \
-    --mount source=/jenkins_home,target=/var/jenkins_home \
-    --mount source=/jars,target=/jars \
+    --mount type=volume,source=jenkins_home,target=/var/jenkins_home \
+    --mount type=volume,source=jars,target=/jars \
     --name styleconnect-jenkins styleconnect-jenkins
