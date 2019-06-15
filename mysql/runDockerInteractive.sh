8331a2ee60cc84989991ab1c51e756c66f7f9144
Josh Benton <tbsw94@gmail.com>
2019-06-15 16:42:54 -0400

    read removed $

--- mysql/runDockerInteractive.sh
+++ mysql/runDockerInteractive.sh
@@ -1,8 +1,8 @@
-!#/bin/bash/
+#!/bin/bash/
 echo "Set the mysql root password"
-read $password
+read msqlpass
 
 docker run  -it -p 3306:3306 \
-    -e MYSQL_ROOT_PASSWORD=$password \
+    -e MYSQL_ROOT_PASSWORD=$msqlpass \
     --mount type=volume,source=mysql,target=/var/lib/mysql \
     --name styleconnect-mysql styleconnect-mysql
