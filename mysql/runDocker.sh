7bac645ff090f123991fff0cbe3018ee9926d655
Josh Benton <tbsw94@gmail.com>
2019-06-17 10:14:51 -0400

    run docker now requires credentials for mysql

--- mysql/runDocker.sh
+++ mysql/runDocker.sh
@@ -1,3 +1,8 @@
+
+echo "Set the mysql root password"
+read msqlpass
+
 docker run  -d -p 3306:3306 \
+    -e MYSQL_ROOT_PASSWORD=$msqlpass \
     --mount type=volume,source=mysql,target=/var/lib/mysql \
     --name styleconnect-mysql styleconnect-mysql
--- mysql/runDockerInteractive.sh
+++ mysql/runDockerInteractive.sh
@@ -2,7 +2,9 @@
 echo "Set the mysql root password"
 read msqlpass
 
-docker run  -it -p 3306:3306 \
+docker run  -d -p 3306:3306 \
     -e MYSQL_ROOT_PASSWORD=$msqlpass \
     --mount type=volume,source=mysql,target=/var/lib/mysql \
     --name styleconnect-mysql styleconnect-mysql
+
+./dockerBash.sh
