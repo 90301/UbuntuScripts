a2ca17c1e3ad759503dad92cd1c1697b9bc6fdb7
Josh Benton <tbsw94@gmail.com>
2019-06-26 11:14:28 -0400

    more duplicati volumes

--- dupliciti/runDocker.sh
+++ dupliciti/runDocker.sh
@@ -4,4 +4,6 @@
 docker run  -d -p 8200:8200 \
     --mount type=volume,source=jenkins_home,target=/jenkins \
     --mount type=volume,source=backups,target=/backups \
+    --mount type=volume,source=mysql,target=/mysql \
+    --mount type=volume,source=duplicatiData,target=/data \
     --name $CONTAINER_NAME $CONTAINER_NAME
